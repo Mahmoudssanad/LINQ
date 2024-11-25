0f5ee10668dba90459dbc36bdf8d52fb40676e21
Mahmoudssanad <sanadmahmoud968@gmail.com>
2024-11-25 21:16:26 +0200

    Add: Explain of Defereed Execution

--- Defereed Execution/Defereed Execution/Program.cs
+++ Defereed Execution/Defereed Execution/Program.cs
@@ -4,7 +4,12 @@ List<string> stringList = new List<string>
             { "Yasser Sanad", "Mo Sanad", "Mahmoud Sanad", "Ahmed Sanad", "Mo Saad" };
 
 
-var names = stringList.Where(name => name.Contains("Sanad"));
+// iteration غير لما اعمل عليه execution دا مش هيحصله query ال 
+// .ToList, .ToArray, .ToDictionary and so on .... زي casting بأحد دوال ال casting او اعمله  
+// var names = stringList.Where(name => name.Contains("Sanad"));
+
+var names = stringList.Where(name => name.Contains("Sanad")).ToList();
+// ومش هيشوف الا انا ضيفته او حذفته لانه اتنفذ خلاص ,iteration علطول مستناش ال query نفذ ال casting لما عملتله 
 
 foreach (var name in names)
 {
@@ -17,6 +22,7 @@ stringList.AddRange(new string[] { "Noor Ahmed", "Nabila Sanad" });
 
 foreach (var name in names)
 {
+    // AddRange وال Remove فهو نفذ اصلا فمش هيشوف ال casting الا فوق لان انا عملت iteration هيطلع نفس ال 
     Console.WriteLine(name);
 }
 
