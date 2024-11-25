ed02b2be20482edf91ce3af43b615112fb8a2279
Mahmoudssanad <sanadmahmoud968@gmail.com>
2024-11-25 21:07:11 +0200

    Add: Lesson_02 Defereed Execution

---
+++ Defereed Execution/Defereed Execution/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+List<string> stringList = new List<string>
+            { "Yasser Sanad", "Mo Sanad", "Mahmoud Sanad", "Ahmed Sanad", "Mo Saad" };
+
+
+var names = stringList.Where(name => name.Contains("Sanad"));
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+Console.WriteLine();
+
+stringList.Remove("Mahmoud Sanad");
+stringList.AddRange(new string[] { "Noor Ahmed", "Nabila Sanad" });
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+Console.ReadKey();
